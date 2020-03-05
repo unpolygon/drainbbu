1dae11adaa6e813a4de42c4f7b6a78b8a6be10ef
Unpolygon <saengsetth@outlook.com>
2020-03-05 16:24:17 +0700

    navbar and graph page

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import '../style/NavBar.scss'
 
 const NavBar = () => {
+    useEffect(() => {
+        navSlide();
+    });
+
+    function navSlide(){
+        const burger = document.querySelector('.Burger');
+        // const nav = document.querySelector('.nav-links');
+        // const navLinks = document.querySelectorAll('.nav-links li');
+
+        burger.addEventListener('click',() => {
+            // nav.classList.toggle('nav-active');
+
+            // navLinks.forEach((link, index) => {
+            //     if(link.style.animation){
+            //         link.style.animation = ''
+            //     }
+            //     else link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+1}s`;
+            // })
+
+            burger.classList.toggle('toggle');
+        })
+    }
+
     return(
         <div className='NavBar'>
-            NavBar
+            <a href = "#" className='Logo'>Logo</a>
+            <div className='Menu-toggle'></div>
+            <nav>
+                <ul>
+                    <li><Link to="/" class='Active'>Home</Link></li>
+                    <li><Link to="/Overview" >Overview</Link></li>
+                    <li><Link to="/Graph" >Graph</Link></li>
+                </ul>
+            </nav>
+            <div className='Burger'>
+                    <div className='line1'></div>
+                    <div className='line2'></div>
+                    <div className='line3'></div>
+                </div>
         </div>
     );
 }
---
+++ src/page/Graph.page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Graph from '../component/Graph';
+import SendGraph from '../component/SendGraph';
+
+const GraphPage = () => {
+    return(
+        <div className='GraphPage'>
+             <SendGraph />
+        </div>
+    );
+}
+
+export default GraphPage;
