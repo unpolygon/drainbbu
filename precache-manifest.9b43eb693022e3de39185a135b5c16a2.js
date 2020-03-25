df37d708dbd6e15317c9cc25ce76f541f009cde3
Hero Intouch <Intouch.pr@ku.th>
2020-03-25 13:59:02 +0700

    Updates

---
+++ precache-manifest.9b43eb693022e3de39185a135b5c16a2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c574f5cc75f1cf8f4b17aacc4b9228",
+    "url": "/drainbbu/index.html"
+  },
+  {
+    "revision": "cf20fa673291eed406d9",
+    "url": "/drainbbu/static/css/2.b3a51b11.chunk.css"
+  },
+  {
+    "revision": "bd8e3731191b1beded56",
+    "url": "/drainbbu/static/css/main.1383cf75.chunk.css"
+  },
+  {
+    "revision": "cf20fa673291eed406d9",
+    "url": "/drainbbu/static/js/2.3dd78b25.chunk.js"
+  },
+  {
+    "revision": "4e2472dac4a6213882d4d80fb8ac2555",
+    "url": "/drainbbu/static/js/2.3dd78b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8e3731191b1beded56",
+    "url": "/drainbbu/static/js/main.fca6e4d0.chunk.js"
+  },
+  {
+    "revision": "9d77638bd4cdc8bd4f36",
+    "url": "/drainbbu/static/js/runtime-main.2218cf2e.js"
+  },
+  {
+    "revision": "244a536a3ded1dd661c1bb3a553a149e",
+    "url": "/drainbbu/static/media/Frame.244a536a.png"
+  },
+  {
+    "revision": "312ab15cdf36d698040fe372e61e7afb",
+    "url": "/drainbbu/static/media/Water7.312ab15c.png"
+  },
+  {
+    "revision": "49ce0e7a3d5731007550a2a54187a314",
+    "url": "/drainbbu/static/media/bbu.49ce0e7a.PNG"
+  },
+  {
+    "revision": "aaba0aaa3487a0513c303ab5ff772872",
+    "url": "/drainbbu/static/media/flood.aaba0aaa.jpg"
+  }
+]);
